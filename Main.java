11771c37f66bf9b3ca44206e8a35fc848df4e949
Anna Talstaya <an.t98@yandex.ru>
2020-11-23 11:03:43 +0300

    Added add for 3 param. Changed first line on method with 2 params

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 class Main {
 	public int add(int a, int b) {
-		System out (“This is add method”);
+		System out (“This is add for 2 params method”);
+		return a+b;
+	}
+	
+	public int add(int a, int b, int c) {
+		System out (“This is add method for 3 params”);
 		return a+b;
 	}
 }
