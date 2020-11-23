eb1c5e187788b7cc501999d00b097331d7325938
Anna Talstaya <an.t98@yandex.ru>
2020-11-23 09:51:40 +0300

    Added sub,div,mul,sqrt methods
     Please enter the commit message for your changes. Lines starting

--- Main.java
+++ Main.java
@@ -2,4 +2,20 @@ class Main {
 	public int add(int a, int b) {
 		return a+b;
 	}
+	
+	public int sub(int a, int b) {
+		return a-b;
+	}
+	
+	public int div(int a, int b) {
+		return a/b;
+	}
+	
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	
+	public int sqrt(int a) {
+		return Math.pow(a, 1/2);
+	}
 }
